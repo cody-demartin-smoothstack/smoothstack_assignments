6f2c1c9120ad7d990203b678a2c3e1457cf5bb82
cody-demartin-smoothstack <cody.demartin@smoothstack.com>
2021-03-10 19:51:56 -0500

    solves day three question one

---
+++ March-10-2021/src/com/cdemarti/assignmentthree/firstquestion/FileSearch.java
@@ -0,0 +1,39 @@
+package com.cdemarti.assignmentthree.firstquestion;
+
+
+import java.io.File;
+
+public class FileSearch {
+    public static void main(String[] args) {
+        String mainPath = "/Users/codydemartin/Desktop";
+        FileSearch search = new FileSearch();
+        File mainDirectory = new File(mainPath);
+
+        if(mainDirectory.exists() && mainDirectory.isDirectory()){
+            File arr[] = mainDirectory.listFiles();
+
+            System.out.println("Files from directory: " + mainPath);
+            search.printFiles(arr, 0, 0);
+        }
+    }
+
+    private void printFiles(File[] arr, int index, int level){
+
+        if(index == arr.length)
+            return;
+
+        for (int i =0; i < level; i++){
+            System.out.print("\t");
+        }
+
+        if(arr[index].isFile()){
+            System.out.println(arr[index].getName());
+        }
+        else if(arr[index].isDirectory()){
+            System.out.println("(" + arr[index].getName() + ")" + " - Directory");
+            printFiles(arr[index].listFiles(), 0, level + 1);
+        }
+
+        printFiles(arr, ++index, level);
+    }
+}
